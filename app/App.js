3400dfef593403356a53adb07c7286db1f451c36
Shaurya Jain <shauryajain19052003@gmail.com>
2026-02-28 18:36:06 +0530

    feat: add app/App.js

---
+++ app/App.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import ChatScreen from './ChatScreen';
+// import ResumeScreen from './ResumeScreen';
+// 
+// const Tab = createBottomTabNavigator();
+// 
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Chat" component={ChatScreen} />
+//         <Tab.Screen name="Resume" component={ResumeScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+// 
+// export default App;
